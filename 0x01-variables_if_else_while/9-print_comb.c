4ade1019ad909167d3adc19a9d76dc2f558ffd06
Amari Kotze <amarikotze@gmail.com>
2023-02-17 13:27:06 -0800

    9 fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,15 +8,21 @@
 int main(void)
 {
 int i;
-int r;
+//int r;
 
 for (i = '0'; i <= '9'; i++)
 {
-for (r = '0'; r <= '9'; i++)
-{
 putchar(i);
-putchar(r);
+if (i != '9')
+{
+putchar(',');
+putchar(' ');
 }
+//for (r = '0'; r <= '9'; i++)
+//{
+//putchar(i);
+//putchar(r);
+//}
 }
 putchar('\n');
 return (0);
