1b29dea4064ef973c9a1f6d3b332f6cd9b16b1db
Amari Kotze <amarikotze@gmail.com>
2023-02-17 11:04:05 -0800

    1 changed way I assigned last

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,8 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
 
-int last = n % 10;
+int last;
+last = n % 10;
 
 if (last < 6 && last != 0)
 printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
