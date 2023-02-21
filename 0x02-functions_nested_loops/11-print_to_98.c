7ff92609acb2748b576a0b2513118870465ceb4b
Amari Kotze <amarikotze@gmail.com>
2023-02-21 13:51:03 -0800

    11 count to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_to_98 - prints numbers from n until 98
+ *@n: start number
+ *Return: void
+ */
+
+void print_to_98(int n)
+{
+int c;
+
+if (n < 98)
+{
+for (c = n; c <= 98; c++)
+{
+printf("%i, ", c);
+}
+}
+else
+{
+for (c = n; n >= 98; c--)
+{
+printf("%i, ", c);
+}
+}
+}
