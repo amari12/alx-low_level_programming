8e298ff15b41fd40424c3ba7298a54a60a8918b1
Amari Kotze <amarikotze@gmail.com>
2023-02-21 10:57:10 -0800

    4 is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_isalpha - checks if a char is a letter, lowercase or uppercase
+ *@c: entered char
+ *Return: int
+ */
+
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z' && c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+
+}
+
