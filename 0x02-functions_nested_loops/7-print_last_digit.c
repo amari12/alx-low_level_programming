33fb34de1fcb4828fa55675d712d24c52c4b7d76
Amari Kotze <amarikotze@gmail.com>
2023-02-22 11:11:10 -0800

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * print_last_digit - prints last digit of number
@@ -16,6 +17,8 @@ if (i < 0)
 last = (i % 10) * -1;
 else
 last = i % 10;
+
+last = abs(last);
 
 return (last);
 }
