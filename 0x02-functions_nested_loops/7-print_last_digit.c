5d83fc87e1cfe57515934135fdeaa44b0d35084e
Amari Kotze <amarikotze@gmail.com>
2023-02-21 22:24:24 -0800

    7 fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,13 +11,14 @@ int print_last_digit(int i)
 char last;
 
 last = i % 10;
+
 /* if the number is negative */
-if (last < '0')
+if (i < '0')
 last = last * -1;
 
 /* add '0') to make it work as a char? */
-_putchar(last);
-_putchar(last);
+_putchar(last + '0');
+/*_putchar(last);*/
 
 return (0);
 }
