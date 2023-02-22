075ffb55d135c2e05370bcf3595cbc5bde7046a3
Amari Kotze <amarikotze@gmail.com>
2023-02-22 11:24:01 -0800

    concat

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * print_last_digit - prints last digit of number
@@ -11,6 +12,8 @@
 int print_last_digit(int i)
 {
 int last;
+char s[10];
+char s2[10];
 
 /* if the number is negative */
 if (i < 0)
@@ -18,7 +21,11 @@ last = (i % 10) * -1;
 else
 last = i % 10;
 
-last = abs(last);
+sprintf(s2, "%d", last);
+sprintf(s, "%d", last);
+strcat(s, s2);
+ 
+last = atoi(s);
 
 return (last);
 }
