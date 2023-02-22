1c2300f983bff554e4ffe3dba975829aa5ce26c5
Amari Kotze <amarikotze@gmail.com>
2023-02-22 10:27:38 -0800

    9 fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,27 +12,27 @@ int row;
 int col;
 int v;
 
-for (row = 0; row < 11; row++)
+for (row = 0; row <= 9; row++)
 {
 
-for (col = 0; col < 11; col++)
+for (col = 0; col < 9; col++)
 {
 v = col * row;
 
 if (col == 0)
 printf("%d,", v);
 
-else if (col < 10 && v < 10)
+else if (col < 9 && v < 10)
 /* not last number in row and only one digit*/
 printf("  %d,", v);
 
-else if (col == 10 && v < 10)
+else if (col == 9 && v < 10)
 printf("  %d\n", v);
 
-else if (col == 10 && v >= 10)
+else if (col == 9 && v >= 10)
 printf(" %d,", v);
 
-else if (col < 10 && v >= 10)
+else if (col < 9 && v >= 10)
 printf(" %d,", v);
 }
 
