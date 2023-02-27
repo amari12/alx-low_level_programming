a74de3850fe40ea2a656db935f1fdd24c3ed538d
Amari Kotze <amarikotze@gmail.com>
2023-02-27 12:01:49 -0800

    8 array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -17,8 +17,8 @@ for (i = 0; i < n; i++)
 if (i < (n - 1))
 printf("%d, ", a[i]);
 else if (i == (n - 1))
-printf("%d\n", a[i]);
-else if (n == 0)
-printf("\n\n");
+printf("%d", a[i]);
 }
+printf("\n");
+
 }
