2be51601c66b04a1303565dbd0ef7989b3e3c42b
Amari Kotze <amarikotze@gmail.com>
2023-02-27 10:53:59 -0800

    7 puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,12 +16,12 @@ len = strlen(str);
 
 if (len % 2 == 0)
 {
-for (i = len / 2; i <= len; i++)
+for (i = len / 2; i < len; i++)
 _putchar(str[i]);
 }
 else
 {
-for (i = (len - 1) / 2; i <= len; i++)
+for (i = (len - 1) / 2; i < len; i++)
 _putchar(str[i]);
 }
 _putchar('\n');
