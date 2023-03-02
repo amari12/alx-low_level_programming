3d968523dd5e5b7e5a893d306f1d5782943deb51
Amari Kotze <amarikotze@gmail.com>
2023-03-01 23:35:14 -0800

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,19 +11,20 @@
 int _strcmp(char *s1, char *s2)
 {
 int result;
-int len1;
-int len2;
-int total;
+/*int len1;*/
+/*int len2;*/
+/*int total;*/
 
-len1 = strlen(s1);
-len2 = strlen(s2);
-result = len1 - len2;
-total = sizeof(s1) + sizeof(s2) - 1;
+/*len1 = strlen(s1);*/
+/*len2 = strlen(s2);*/
+/*result = len1 - len2;*/
+/*total = sizeof(s1) + sizeof(s2) - 1;*/
+result = s1 - s2;
 
 if (result < 0)
-return (-total);
+return (result);
 else if (result > 0)
-return (total);
+return (result);
 else
 return (0);
 
