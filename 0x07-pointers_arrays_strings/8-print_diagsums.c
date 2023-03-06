6ad018439d8ebe41a20621686c0323127b0c4783
Amari Kotze <amarikotze@gmail.com>
2023-03-06 11:44:49 -0800

    diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -20,12 +20,12 @@ sum2 = 0;
 
 for (diag1 = 0; diag1 < size; diag1++)
 {
-sum1 = sum1 + a[diag1];
+sum1 = sum1 + a[diag1 * size + diag1];
 }
 
 for (diag2 = size - 1; diag2 >= 0; diag2--)
 {
-sum2 = sum2 + a[diag2];
+sum2 = sum2 + a[size - diag2 - 1];
 }
 printf("%d, %d\n", sum1, sum2);
 }
