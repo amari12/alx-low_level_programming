c598fcdf0db022ca32ac3ed38919abc4b246457f
Amari Kotze <amarikotze@gmail.com>
2023-03-06 10:32:55 -0800

    5 strstr

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strpbrk - search string for set of bytes
+ *@s: string
+ *@accept: bytes
+ *Return: char pointer to byte in s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+int is;
+int ia;
+int lens;
+int lena;
+int match;
+
+lens = strlen(s);
+lena = strlen(accept);
+match = 0;
+ia = 0;
+is = 0;
+while (is < lens && match != 1)
+{
+while (ia < lena && match != 1)
+{
+if (s[is] == accept[ia])
+match = 1;
+else (s[is] != accept[ia])
+ia++;
+}
+is++;
+}
+return (s[ia]);
+}
