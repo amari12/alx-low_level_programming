56761c84fbfa42b5ac4bd3cd6ac06051685f1b8e
Amari Kotze <amarikotze@gmail.com>
2023-03-14 12:15:04 -0700

    3 grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,28 +21,19 @@ return (NULL);
 arr = (int **)malloc(height * sizeof(int));
 
 if (arr)
-{/*check if malloc successful*/
-for (row = 0; row < height; row++)
-{/*assign each row /1D array a pointer?*/
-arr[row] = (int *)malloc(width * sizeof(int));
-if (arr[row] == NULL)
-{/*check if malloc successful/failed*/
-free(arr[row]);
-free(arr);
-return (NULL);
-} /*end if*/
-} /*end for row*/
-
-/*assign 0s*/
+{/*check if it worked then initialise with 0s*/
 for (row = 0; row < height; row++)
 {
+/*assign each row /1D array a pointer?*/
+arr[row] = (int *)malloc(width * sizeof(int));
+
 for (col = 0; col < width; col++)
 {
 arr[row][col] = 0;
 }
 }
 return (arr);
-} /*end of if(arr)*/
+}
 
 else
 {/*return null on failure*/
