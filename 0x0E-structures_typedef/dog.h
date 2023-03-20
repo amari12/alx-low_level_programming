264a57f7f2803c8cd454ea66d74a1c3bd14060a0
Amari <amari.kotze@gmail.com>
2023-03-20 04:27:11 -0700

    0 betty comment block

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ *struct dog - dog details
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of dog
+ *Description: dog details
+ */
+
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 };
 
 typedef struct dog dog_t;
