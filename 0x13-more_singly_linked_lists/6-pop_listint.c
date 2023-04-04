6e02221995fa1e5f739acc1d67cc31893035ca70
AmariKotze <amarikotze@gmail.com>
2023-04-04 11:44:34 -0700

    6 pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
  *pop_listint - remove head node
@@ -9,15 +10,19 @@
 int pop_listint(listint_t **head)
 {
 listint_t *temp;
+int nr;
 
 /*check if list empty*/
 if (head == NULL || *head == NULL)
 return (0);
 
-/*save value*/
-temp = *head;
-/*make next node the new head*/
-*head = (*head)->next;
+/*save temp values*/
+temp = (*head)->next;
+nr = (*head)->n;
+/*free space*/
+free(*head);
+/*assign new head*/
+*head = temp;
 /*return removed head's value*/
-return (temp->n);
+return (nr);
 }
