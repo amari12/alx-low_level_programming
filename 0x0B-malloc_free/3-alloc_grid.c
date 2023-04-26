cbcc9e23ba2bdae402cb7cc98d75b3fb28c0fe97
Amari <amarikotze@gmail.com>
2023-04-26 12:22:23 -0700

    task3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,20 +15,25 @@ int row, col, i;
 int **arr;
 
 /*check if height/width is zero*/
-if (width == 0 || height == 0)
-return (NULL);
-arr = malloc(height * sizeof(int));
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	arr = malloc(height * sizeof(int *));
+	if (arr == NULL)
+		return (NULL);
+
 for (row = 0; row < height; row++)
 {
 /*assign each row /1D array a pointer?*/
 arr[row] = malloc(width * sizeof(int));
 if (arr[row] == NULL)
-{
+{ /*free prev assigned memory*/
 	for (i = 0; i < row; i++)
 		free(arr[i]);
 	free(arr);
 	return (NULL);
 }
+/*intialise all elements with 0*/
 for (col = 0; col < width; col++)
 {
 arr[row][col] = 0;
