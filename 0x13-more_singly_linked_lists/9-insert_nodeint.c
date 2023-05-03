906fe39ad957a422aef83855ada7f7dcea2b2272
Amari <amarikotze@gmail.com>
2023-05-03 07:04:40 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	/*assign value*/
 	new->n = n;
 	/*check if list is empty or if idx is 0*/
-	if (*head == NULL && idx == 0)
+	if ((*head == NULL || head == NULL) && idx == 0)
 	{
 		*head = new;
 		return (new);
