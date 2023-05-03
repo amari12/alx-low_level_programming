a5bded6907eed63e1f50b7134409f63c30ac0e1d
Amari <amarikotze@gmail.com>
2023-05-03 07:08:10 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -43,13 +43,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		else
 		{
-			free(new);
 			temp = current;
 			current = current->next;
 			prev = temp;
 		}
 	}
 	/*could not find index*/
+	free(new);
 	return (NULL);
 }
 
