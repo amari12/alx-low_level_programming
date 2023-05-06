78c8cb7633117d2e711b95bfeb772390d995e02d
Amari <amarikotze@gmail.com>
2023-05-06 13:41:26 -0700

    task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets value of bit to 0 at index
+ * @n: number
+ * @index: index
+ * Return: int (1 succes, -1 fail)
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	*n |= ~(1UL << index);
+	/* ~ inverts*/
+	/* (1UL << index) = bitmask has 1 at index*/
+	/* rest are 0s*/
+	return (1);
+}
