fd97e177d4d7e0f2771f65a0fa3355cea02dc61b
Amari <amarikotze@gmail.com>
2023-05-06 13:21:21 -0700

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,6 +11,13 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit;
 
+	/*check if index is valid*/
+	/*needs to be smaller than 8 times the size of uli*/
+	/*unsigned long int is 1 byte*/
+	/* 8 bits in a byte*/
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
 	bit = n >> index; /*gets the bit at that index*/
 	if (bit & 1) /*if binary ends in 1*/
 		return (1);
