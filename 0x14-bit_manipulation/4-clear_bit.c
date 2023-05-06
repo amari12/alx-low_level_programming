dfae8ac28d21e6c2cd51cbb29b32401bf336b0bc
Amari <amarikotze@gmail.com>
2023-05-06 13:42:39 -0700

    task 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,7 +12,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	*n |= ~(1UL << index);
+	*n &= ~(1UL << index);
 	/* ~ inverts*/
 	/* (1UL << index) = bitmask has 1 at index*/
 	/* rest are 0s*/
