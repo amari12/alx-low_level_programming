791e5b5f0c9820cba0034e46a676c2c66bb7af1f
Amari <amarikotze@gmail.com>
2023-05-06 12:46:32 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,6 +12,7 @@ unsigned int binary_to_uint(const char *b)
 {
 	int i, len = 0;
 	unsigned int nr = 0;
+	/*char *ptr;*/
 
 	/*checks*/
 	if (b == NULL)
@@ -21,13 +22,13 @@ unsigned int binary_to_uint(const char *b)
 	len = strlen(b);
 	for (i = 0; i < len; i++)
 	{
-		if (b[i] != 1 || b[i] != 0)
+		if (b[i] != 1 && b[i] != 0)
 			return (0);
 		nr = (nr << 1) + (b[i] + '0'); /*convert*/
 	}
 	/*convert*/
-	/*nr = strtoul(b, NULL, 2);*/
-	
+	/*nr = strtoul(b, &ptr, 2);*/
+
 	return (nr);
 }
 
