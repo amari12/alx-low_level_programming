d550423ba1055bedd33d1ed01bd76a295440a3dd
Amari <amarikotze@gmail.com>
2023-05-08 12:03:55 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,12 +12,11 @@
 int create_file(const char *filename, char *text_content)
 {
 	size_t len, written;
-	
+
 	FILE *p_file;
 
 	if (filename == NULL || text_content == NULL)
 		return (-1);
-
 	p_file = fopen(filename, "w");
 
 	if (p_file == NULL)
@@ -26,9 +25,9 @@ int create_file(const char *filename, char *text_content)
 		fclose(p_file);
 		return (-1);
 	}
-	
 	len = strlen(text_content); /*get length of give text*/
-	written = fwrite(text_content, 1, len, p_file); /*check how many chars were written*/
+	written = fwrite(text_content, 1, len, p_file);
+	/*check how many chars were written*/
 	if (written != len)
 	{ /*did not write full text*/
 		fclose(p_file);
